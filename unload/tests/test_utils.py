524fd4f7dea156c9f0a094f6179b4ddce39b169e
Filip Todic <filip.todic@styria.hr>
2016-08-29 16:26:05 +0200

    Add a demo project, a simple test, travis and tox config files

---
+++ unload/tests/test_utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+
+class TestUtils(TestCase):
+
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
