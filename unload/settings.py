fcd8b003874f499b586f83f162a43f1ea8cd0005
Filip Todic <filip.todic@styria.hr>
2017-10-12 16:21:53 +0200

    Test the Python's version using six

--- unload/settings.py
+++ unload/settings.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import sys
-
-from django import get_version
 from django.template.defaultfilters import register as filter_lib
 from django.templatetags.cache import register as cache_lib
 from django.templatetags.i18n import register as i18n_lib
 from django.templatetags.l10n import register as l10n_lib
 from django.templatetags.static import register as static_lib
 from django.templatetags.tz import register as tz_lib
-
-# The future templatetags module was removed in Django 1.10
-try:
-    from django.templatetags.future import register as future_lib
-    FUTURE_FILTERS = future_lib.filters.keys()
-except ImportError:
-    FUTURE_FILTERS = {}
-
-PYTHON_VERSION = sys.version_info
-DJANGO_VERSION = get_version()
+from django.utils import six
 
 BUILT_IN_FILTERS = filter_lib.filters.keys()
 CACHE_FILTERS = cache_lib.filters.keys()
@@ -95,13 +83,13 @@ STATIC_TAGS = {
     'get_static_prefix': None
 }
 
-if PYTHON_VERSION.major == 2:
+if six.PY2:
     BUILT_IN_TAG_VALUES = set(BUILT_IN_TAGS.values() +
                               I18N_TAGS.values() +
                               L10N_TAGS.values() +
                               CACHE_TAGS.values() +
                               STATIC_TAGS.values())
-elif PYTHON_VERSION.major == 3:
+elif six.PY3:
     BUILT_IN_TAG_VALUES = set(list(BUILT_IN_TAGS.values()) +
                               list(I18N_TAGS.values()) +
                               list(L10N_TAGS.values()) +
